75f087406b698411b972421e15688aea6303d4e1
Nisharani26 <147154441+Nisharani26@users.noreply.github.com>
2025-07-06 03:45:09 +0530

    Update app.py

--- Fake News Verifier/app.py
+++ Fake News Verifier/app.py
@@ -191,6 +191,6 @@ def verify_news(input_text, language):
     return result
 
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=True)
